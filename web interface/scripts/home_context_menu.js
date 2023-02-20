6a0311e9c0f7c9dc2e0e205bcfcfd8e4e2331276
Arman <82840553+Arman-sm@users.noreply.github.com>
2023-02-20 18:22:49 +0330

    Improved some icons and added more effects

--- web interface/scripts/home_context_menu.js
+++ web interface/scripts/home_context_menu.js
@@ -5,14 +5,15 @@ let isMouseDown = false
 document.body.onmouseup = event => {isMouseDown = false; if (event.target !== menu) {menu.classList.toggle("invisible", true)}}
 menu.onmouseup = event => {event.target.click(); menu.classList.toggle("invisible", true)}
 
-function showMenu(top, left, audioID, borderRadius, height, width) {
+function showMenu(top, left, ID, borderRadius, height, width, type) {
 	menu.style.top = top + "px"
 	menu.style.left = left + "px"
 	menu.style.borderRadius = borderRadius
 	menu.style.height = height
 	menu.style.width = width
 	
-	menu.setAttribute("data-id", audioID)
+	menu.setAttribute("data-id", ID)
+	menu.setAttribute("data-item-type", type)
 	
 	menu.classList.toggle("invisible", false)
 }
@@ -20,11 +21,13 @@ function showMenu(top, left, audioID, borderRadius, height, width) {
 function showFloatingMenuOnContext(event) {
 	if (event.ctrlKey) return
 	event.preventDefault()
-	showMenu(event.clientY, event.clientX, event.target.getAttribute("data-id") || event.target.parentElement.getAttribute("data-id"), "0.75rem", "auto", "auto")
+	const ID = event.target.getAttribute("data-id") || event.target.parentElement.getAttribute("data-id")
+	const type = event.target.getAttribute("data-id") || event.target.parentElement.getAttribute("data-item-type")
+	showMenu(event.clientY, event.clientX, ID, "0.75rem", "auto", "auto", type)
 }
 
-function showContextOnElement(element) {
-	const { height, width, borderRadius } = getComputedStyle(element)
-	const { x, y } = element.getBoundingClientRect()
-	showMenu(x, y, element.getAttribute("data-id"), borderRadius, height, width)
-}
+// function showContextOnElement(element) {
+// 	const { height, width, borderRadius } = getComputedStyle(element)
+// 	const { x, y } = element.getBoundingClientRect()
+// 	showMenu(x, y, element.getAttribute("data-id"), borderRadius, height, width)
+// }
